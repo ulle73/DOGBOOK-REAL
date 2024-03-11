4d43d81f05865430dba21f76c0af62961280f75b
ulle73 <rydqvist89@hotmail.com>
2024-03-11 01:39:04 +0100

    skapat css-profile-cards

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,15 +3,16 @@ import { useParams, Link } from 'react-router-dom'
 import axios from 'axios'
 
 
-function Profile({dogs}) {
+function Profile({ dogs }) {
     const { id } = useParams()
     console.log("ID:", id, typeof id)
 
     const [dogImage, setDogImage] = useState("")
+    const [friendDogImage, setFriendDogImage] = useState("")
     const [dog, setDog] = useState({
 
     })
- 
+
 
     useEffect(() => {
         async function fetchDog() {
@@ -19,6 +20,11 @@ function Profile({dogs}) {
                 const get = await fetch("https://dog.ceo/api/breeds/image/random")
                 const data = await get.json()
                 setDogImage(data.message)
+
+                const friendGet = await fetch("https://dog.ceo/api/breeds/image/random")
+                const friendData = await friendGet.json()
+                setFriendDogImage(friendData.message)
+
 
                 const response = await axios.get(`http://localhost:3000/dogs/${id}`)
                 console.log("Resp", response)
@@ -33,31 +39,61 @@ function Profile({dogs}) {
     }, [id])
 
 
-    return (
-        <div>
-            <h2>{dog.name} Profile</h2>
-            <img className="dog-image" src={dogImage} />
-            <p>Nickname: {dog.nickname}</p>
-            <p>Owner: {dog.owner}</p>
-            <p>Age: {dog.age}</p>
-            <p>Bio: {dog.bio}</p>
-            <p>Present: {dog.present ? 'Yes' : 'No'}</p>
-            <p>Friends: </p>
 
-            <ul>
-  {dog.friends && dog.friends.length > 0 ? 
-    dog.friends
-      .filter(friend => dogs.some(dog => dog._id === friend._id))
-      .map((friend, index) => (
-        <li key={index}><Link to={`/profile/${friend._id}`}>{friend.name}</Link></li>
-      )) 
-    : 'No friends'}
-</ul>
+
+
+
+
+    return (
+        <div className="container">
+            <img className="dog-image center" src={dogImage} />
+            <h1 className="center dog-name">{dog.name} </h1>
+            <p className="center nickname"> {dog.nickname}</p>
+            {/* <p >Owner: {dog.owner}</p> */}
+            <span className="flex"><i className="fa-solid fa-cake-candles fa-xl center cake"></i><h1 className="center age">{dog.age}</h1></span>
+
+            <p className="center bio">{dog.bio}</p>
+            <p className="center">Present: {dog.present ? 'Yes' : 'No'}</p>
+            {/* <span className="flex"><p>Friends: </p>
+
+<ul>
+{dog.friends && dog.friends.length > 0 ? 
+dog.friends
+.filter(friend => dogs.some(dog => dog._id === friend._id))
+.map((friend, index) => (
+<div key={index}>
+<img className="dog-image-friend" src={dogImage}  />
+<li><Link to={`/profile/${friend._id}`}>{friend.name}</Link></li>
+</div>
+)) 
+: 'No friends'}
+</ul></span>  */}
+<div> <hr /></div>
+
+            {/* <p>Friends: </p> */}
+
+            <div className="flex friends">
+                {dog.friends && dog.friends.length > 0 ?
+                    dog.friends
+                        .filter(friend => dogs.some(dog => dog._id === friend._id))
+                        .map((friend, index) => (
+                            <div key={index}>
+                                <div className="center">
+                                    <Link to={`/profile/${friend._id}`}>
+                                        <img className="dog-image-friend" src={friendDogImage} />
+                                        <div>{friend.name}</div>
+                                    </Link>
+                                </div>
+                            </div>
+                        ))
+                    : 'No friends'}
+            </div>
+
 
 
 
             <Link to={`/edit/${id}`}>Edit</Link>
-            <br />
+          
             <br />
             <Link to="/">Back</Link>
         </div>
